f9e6d60010d83efb565dd595f052931fb9ed9caa
faspen <aspensonf@gmail.com>
2020-12-19 20:40:19 +0000

    why would you want a function that does nothing???

--- monty.h
+++ monty.h
@@ -40,6 +40,7 @@ extern int main_int;
 
 void _push(stack_t **stack, unsigned int line_number);
 void _pall(stack_t **stack, unsigned int line_number);
-void _pint(stack_t **stack, unsigned int line_number)
+void _pint(stack_t **stack, unsigned int line_number);
+void _nop(stack_t **stack, unsigned int line_number);
 
 #endif
---
+++ nop.c
@@ -0,0 +1,12 @@
+#include "monty.h"
+
+/**
+* _nop - literally doesn't do anything XD
+* @stack: what's the point?
+* @line_number: what's the point?
+*/
+void _nop(stack_t **stack, unsigned int line_number)
+{
+	(void)stack;
+	(void)line_number;
+}
