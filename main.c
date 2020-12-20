943fc7f3bfd8acd8dbdfc847b7fb9a448bbf95e2
faspen <aspensonf@gmail.com>
2020-12-20 15:41:42 +0000

    added main file

---
+++ main.c
@@ -0,0 +1,26 @@
+#include "monty.h"
+
+/**
+* main - main file of monty interpreter
+* @argc: argument number
+* @argv: arguments
+* Return: success or fail
+*/
+int main(int argc, char *argv[])
+{
+	stack_t **stack;
+
+	stack = malloc(sizeof(stack_t));
+	*stack = NULL;
+
+	if (argc >= 2 && argc <= 3)
+		inspect(argv[1], stack);
+	else
+	{
+		fprintf(stderr, "USAGE: monty file\n");
+		myfree(stack);
+		exit(EXIT_FAILURE);
+	}
+	myfree(stack);
+	return (0);
+}
