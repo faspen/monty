4db5e2caa76608b6a79e7d47d90893411744c769
faspen <aspensonf@gmail.com>
2020-12-22 23:00:16 +0000

    removed uneeded error message

--- inspect.c
+++ inspect.c
@@ -7,20 +7,20 @@
 void inspect(char *argstring)
 {
 	stack_t *stack = NULL;
-	FILE *file;
+	FILE *file; /*the file that is being opened */
 	int count = 0;
 	char *line = NULL, **argues;
 	size_t buf = 0;
 	void (*f)(stack_t **stack, unsigned int line_number);
 
-	file = fopen(argstring, "r");
+	file = fopen(argstring, "r"); /* open file to read */
 
 	if (!file)
 	{
 		fprintf(stderr, "Error: Can't open file %s\n", argstring);
 		exit(EXIT_FAILURE);
 	}
-	while (getline(&line, &buf, file) != -1)
+	while (getline(&line, &buf, file) != -1) /* loop that goes through the file */
 	{
 		count++;
 		argues = split(line); /* tokenize */
@@ -33,7 +33,6 @@ void inspect(char *argstring)
 			f(&stack, count);
 		else
 		{
-			fprintf(stderr, "L%d: usage: push integer\n", count);
 			fclose(file);
 			myfree(&stack);
 			free(line);
